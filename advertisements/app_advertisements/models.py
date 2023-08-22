5cf0cf5df86a6658e00cf9c064d64738932045fe
vakilB <bvakil124@gmail.com>
2023-08-22 11:59:32 +0300

    dz

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.utils.html import format_html
 from django.contrib.auth import get_user_model
 
+
+
 User = get_user_model()
 
 # Create your models here.
@@ -17,8 +19,11 @@ class Advertisements(models.Model):
     user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
     image = models.ImageField('Изображение', upload_to="advertisements/", blank=True)
 
+    class Meta:
+        db_table = "advertisements"
     def __str__(self):
-        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price}, user= {self.user}, image = {self.image})"
+
 
     @admin.display(description='дата создания')
     def created_date(self):
@@ -43,12 +48,12 @@ class Advertisements(models.Model):
         return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
     @admin.display(description='Картинка')
     def img(self):
-
-
-        return format_html(
-            '<img src="{{self.image.url}}">'
-
+        if self.image:
+            return  format_html(
+                '<img src="{url}" style="max-widht: 80px; max-height:80px">', url=self.image.url
             )
 
-class Meta:
-    db_table = "advertisements"
+
+
+
+
