86068fb4b7c59ff6cb85aa6962da127f2795a9cf
Vakil <burangulov.vakil@gmail.com>
2024-02-14 14:43:53 +0300

    Telebot

---
+++ test.py
@@ -0,0 +1,16 @@
+# import telebot
+# from telebot import types
+# token = '5993234240:AAHEqBrRMds1oP-JcdsYgCrQ24tSW4bdnu4'
+
+# bot = telebot.TeleBot(token)
+with open('test.txt') as f:
+    file = open('test.txt', 'w+')
+    file.write('сосиска\n')
+    file.write('картошка')
+    x = file.read()
+
+
+
+
+
+# bot.polling()
